70b89e1f7531ef8a77c49cf126b6a65155918af7
cameza <camilo.meza@me.com>
2015-03-01 02:39:12 +0000

    added the nominee controller and the index view for this controller

---
+++ app/controllers/nominees_controller.rb
@@ -0,0 +1,4 @@
+class NomineesController < ApplicationController
+	def index
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 AOMVotingApp::Application.routes.draw do
+  root 'nominees#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
