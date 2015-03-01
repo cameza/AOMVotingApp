dd0890985af1885e714c7164ec2f68e3892af97d
cameza <camilo.meza@me.com>
2015-03-01 02:28:40 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AOMVotingApp::Application.config.secret_key_base = '2025f46c96fbb7f3a70247fcc5529d7409ebce76f22dbb100b63ff512b3b0ee2c63a6c13d6970d4a0da303ed14f5fe54b2cb8b7e7e82e0f68b828f9e1d4d10db'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AOMVotingApp::Application.config.session_store :cookie_store, key: '_AOMVotingApp_session'
